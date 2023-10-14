e6835d6b38d4b437dde5c768b3fdf9de2061a6d0
Abraão Nascimento <abraaoan@neemu.com>
2023-10-14 15:28:36 +0100

    first commit

---
+++ FinancialApp/FinancialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialAppApp.swift
+//  FinancialApp
+//
+//  Created by Abraao Nascimento on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FinancialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
